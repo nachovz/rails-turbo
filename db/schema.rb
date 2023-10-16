d1bade35b71986b7f35970dbfef0e9b1da96aea4
Ignacio Cordoba <theignaciocordoba@gmail.com>
2023-10-16 19:48:46 +0000

    chore: make quote.name required

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_09_212014) do
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.integer "quote_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
