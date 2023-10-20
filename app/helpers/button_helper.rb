573a62a3784eafcb6c8f7e792283b382ee11ce89
Nacho <theignaciocordoba@gmail.com>
2023-10-20 17:26:35 -0400

    feat: add button helper

---
+++ app/helpers/button_helper.rb
@@ -0,0 +1,18 @@
+module ButtonHelper
+	TYPES = {
+		"primary" => "bg-green-500 hover:bg-green-600",
+		"secondary" => "bg-red-500 hover:bg-red-600",
+		"helper" => "bg-violet-200 hover:bg-violet-300",
+	}
+
+
+	# Usage:
+	# <%= button_link "Sign Up", new_user_registration_path, "secondary" , "data-turbolinks": false, "data-turbo": false, "data-method": "get", "data-action": "click->modal#open", "data-modal-target": "modal" %>
+	def button_link(link_text, link_path, button_type = "primary", **data)
+		puts data
+		button_class = "px-3 py-2 font-bold text-center rounded-md text-white hover:text-white "
+
+		link_to link_text, link_path, class: button_class + TYPES[button_type], data: data
+	end
+
+end
