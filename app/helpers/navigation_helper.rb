bc67245619c777ddf626c484c39ddef81d54d4f2
Nacho <theignaciocordoba@gmail.com>
2023-10-23 16:30:33 -0400

    chore: improve active page helper

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,7 +1,7 @@
 module NavigationHelper
 	def nav_link(link_text, link_path)
 		class_name = "flex-1 px-4 py-3 font-bold text-center "
-		class_name += current_page?(link_path) ? 'border-b-2 border-solid border-b-red-500' : ''
+		class_name += link_path.split("/").include?(controller_name) ? 'border-b-2 border-solid border-b-red-500' : ''
 
 		link_to link_text, link_path, class: class_name
 	end
